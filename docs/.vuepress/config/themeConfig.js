e3d54881dde205db9e90edc6579da1babd092984
lingtaoluo <lingtaoluo@risinghf.com>
2021-06-01 11:09:53 +0800

    修复在线编辑此页跳转失败的bug

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -10,6 +10,7 @@ module.exports = {
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
+  docsBranch: 'main',
   editLinks: true, // 启用编辑
   editLinkText: '编辑',
   locales: {
